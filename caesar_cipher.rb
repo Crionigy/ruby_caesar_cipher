2ef3a137fd84897c96a81307a5aac92c90d5ca5f
Arthur Hassan Souki <souki.arthur@gmail.com>
2021-02-09 21:07:05 -0300

    projeto finalizado sem teste unitario

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,16 @@
 def caeser_cipher(text, key)
     letters = ('a'..'z').to_a
-    
-    #text.chars.map { |char| puts letters.include?(char.downcase) ? letters[letters.index(char) + key - letters.size] : x}
+    encrypted_array = Array.new
+
+    encrypted_array << text.chars.map { |char| letters.include?(char.downcase) ? letters[letters.find_index(char.downcase) + key - letters.size] : char}
+
+    encrypted_array.join
 end
 
 print "Insira uma texto a ser criptografada: "
 text = gets.chomp
 
 print "Insira uma chave para a criptografia: "
-key = gets.chomp
+key = gets.chomp.to_i
 
 puts caeser_cipher(text, key)
